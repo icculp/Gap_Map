ad40a8c46ad2b40ac678d83c0834301a1010f1cf
Ian Culp <icculp@gmail.com>
2020-10-20 21:25:09 +0000

    Adding geocode search feature, cleaning up presentation

--- install.sh
+++ install.sh
@@ -4,9 +4,15 @@
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo apt-get install mysql-server -y
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+
+npm install -g browserify
 
 pip3 install gunicorn
 pip3 install flask
 pip3 install mysqldb
 pip3 install sqlalchemy
+pip3 install flasgger
+pip3 install flask_cors
 
