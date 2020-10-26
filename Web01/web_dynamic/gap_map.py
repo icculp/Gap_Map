b42bfb7e520d3021eb2f933ae6a6b2fe28cd6ff7
Ian Culp <icculp@gmail.com>
2020-10-26 18:19:46 +0000

    Updating content, adjusting css, attempting city boundary line

---
+++ Web01/web_dynamic/gap_map.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+import uuid
+from os import environ
+from flask import Flask, render_template
+app = Flask(__name__)
+import sys
+sys.path.append('/home/vagrant/Gap_Map/Addresses')
+from dhs import scrapeDHS, geo_code_bulk
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/gap_map/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    #addr = scrapeDHS(zip='74115')
+    list_daycares = []
+    #geo_code_bulk(addr)
+    return render_template('101-hbnb.html',
+                           list_daycares=list_daycares,
+                           cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000, debug=True)
